a90127a342cf0577ac3a20d7b88fd59ef882bd5d
RodrigoVG <desqui92@gmail.com>
2022-07-02 18:45:58 -0300

    Cambio css.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
-var slide = document.getElementsByClassName("c-slider-container__slide");
-var sliderl = document.getElementsByClassName("c-slider-container__slider");
-var image = document.getElementsByClassName("c-slider-container__image");
+var slidep = document.getElementsByClassName("c-section-two")[0];
+var slide = slidep.getElementsByClassName("c-slider-container__slide-two");
+
+var imagep = document.getElementsByClassName("c-section-two")[0];
+var image = imagep.getElementsByClassName("c-slider-container__image-two");
+
+var slideptwo = document.getElementsByClassName("c-section-five")[0];
+var slidetwo = slideptwo.getElementsByClassName("c-slider-container__slide-five");
+
+var imageptwo = document.getElementsByClassName("c-section-five")[0];
+var imagetwo = imageptwo.getElementsByClassName("c-slider-container__image-five");
+
+
 var indice = 0;
 
 function slider(indice){
@@ -23,6 +33,29 @@ function slider(indice){
 
 }
 
+function slidertwo(indice){
+    var j = 0;
+    for(let i=0;i<=3;i++){
+        if(indice+i <= indice+2){
+            if(indice+i > 5){
+                slidetwo[j].style.display = "flex";
+                slidetwo[j].style.flexDirection = "column";
+                slidetwo[j].style.order = "3";
+                slidetwo[j].style.alignItems = "center";
+                imagetwo[j].style.display = "block"; 
+                imagetwo[j].style.order = "3";
+                j++;   
+            }else{
+            slidetwo[indice+i].style.display = "flex";
+            slidetwo[indice+i].style.flexDirection = "column";
+            slidetwo[indice+i].style.alignItems = "center";
+            imagetwo[indice+i].style.display = "block";
+            }
+        }
+    }
+
+
+}
 
 
 
@@ -68,6 +101,9 @@ function leftarrow(){
     
 }
 
-window.onload = slider(0);
+function sliders(){
+    slider(0);
+    slidertwo(0);
+}
 
-
+window.onload = sliders();
